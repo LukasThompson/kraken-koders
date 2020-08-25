7306620c9226c9a260c270f1670bd261bafcc43f
Lukas Thompson <lukas.thompson@protonmail.com>
2020-08-25 19:20:57 -0400

    Card, Home Page, Jumbotron

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./App.css";
 import NavbarPage from "./components/NavBarPage";
+import { MDBContainer, MDBCardGroup } from "mdbreact";
+import ProjectCardPage from "./components/ProjectCardPage";
+import JumbotronPage from "./components/JumbotronPage";
 
 function App() {
   return (
     <div className="App">
       <NavbarPage />
+      <MDBContainer>
+        <JumbotronPage />
+        <MDBCardGroup>
+          <ProjectCardPage />
+          <ProjectCardPage />
+          <ProjectCardPage />
+        </MDBCardGroup>
+      </MDBContainer>
       Welcome to Kraken Koders bitch!
     </div>
   );
